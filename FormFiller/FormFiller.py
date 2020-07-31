c342ea055c6eb1d8aa655f25abf95a5e3d4dac33
Lumary2 <bender.anja@gmail.com>
2020-07-31 10:03:12 +0200

    x

--- FormFiller/FormFiller.py
+++ FormFiller/FormFiller.py
@@ -4,14 +4,13 @@ from selenium import webdriver
 driver= webdriver.Firefox()
 driver.get('http://127.0.0.1:5000/')
 
-
 pyautogui.click(611,344)
 pyautogui.typewrite("Hermine" + '\t')
 pyautogui.typewrite("Granger" + '\t')
 pyautogui.typewrite(['down','tab',' '])
 
 pyautogui.press('tab')
-pyautogui.click(654,601)
+#pyautogui.click(654,601)
 
 #close window
 #pyautogui.hotkey('ctrl','q')
